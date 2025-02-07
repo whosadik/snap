e191b573ed417a7a00893035c4cf9b79a8988b3d
whosadik <adikmanmedet@gmail.com>
2025-02-08 00:48:29 +0500

    first commit

--- app.js
+++ app.js
@@ -183,21 +183,223 @@ function playVideoMobile(card) {
   let video = card.querySelector(".video");
 
   if (window.innerWidth <= 768) {
-    // Запускаем видео в полноэкранном режиме на мобильных
-    if (video.requestFullscreen) {
-      video.requestFullscreen();
-    } else if (video.webkitRequestFullscreen) {
-      video.webkitRequestFullscreen();
-    } else if (video.msRequestFullscreen) {
-      video.msRequestFullscreen();
-    }
-
-    video.play();
-  } else {
-    // Для десктопов просто играем видео
     if (video.paused) {
       video.play();
-      card.classList.add("playing");
+
+      // Попытка открыть видео в полноэкранном режиме
+      if (video.requestFullscreen) {
+        video.requestFullscreen();
+      } else if (video.webkitRequestFullscreen) {
+        // iPhone, Safari
+        video.webkitRequestFullscreen();
+      } else if (video.msRequestFullscreen) {
+        // IE/Edge
+        video.msRequestFullscreen();
+      }
     }
+  } else {
+    // Для десктопов видео играет в карточке
+    video.play();
   }
 }
+
+document.querySelectorAll(".video-card").forEach((card) => {
+  card.addEventListener("click", function () {
+    const country = this.dataset.country;
+    const gesture = this.dataset.gesture;
+
+    console.log("Страна:", country);
+    console.log("Жест:", gesture);
+
+    const key = `${country}_${gesture}`;
+    console.log("Сформированный ключ:", key);
+  });
+});
+
+document.querySelectorAll(".video-card").forEach((card) => {
+  card.addEventListener("click", function () {
+    console.log("Все dataset атрибуты:", this.dataset);
+  });
+});
+
+const gestureVariations = {
+  Вкусно: [
+    {
+      country: "Франция",
+      img: "preview/32-FR-Delicious.jpg",
+      desc: "Вкусно; Отлично",
+    },
+    {
+      country: "Португалия",
+      img: "preview/Portuges/19-PG-Delicious.jpg",
+      desc: "Вкусно",
+    },
+    {
+      country: "Италия",
+      img: "preview/28-IT-Delicious.jpg",
+      desc: "Вкусно",
+    },
+  ],
+  Я_знал_это: [
+    {
+      country: "Франция",
+      img: "preview/francais/31-FR-I knew it.jpg",
+      desc: "Я знал это; У меня хорошее чутье",
+    },
+    {
+      country: "Великобритания",
+      img: "preview/21-GB-Don't be nosy.jpg",
+      desc: "Не будьте любопытными; это не твое дело",
+    },
+  ],
+  Ноль: [
+    {
+      country: "Франция",
+      img: "preview/francais/10-FR-Zero.jpg",
+      desc: "Ноль; Ты ничто",
+    },
+    {
+      country: "Италия",
+      img: "preview/11-IT-Perfect.jpg",
+      desc: "Идеально; Отлично",
+    },
+    {
+      country: "Великобритания",
+      img: "preview/british/12-GB-Perfect.jpg",
+      desc: "Идеально; Отлично",
+    },
+  ],
+  Говорил: [
+    {
+      country: "Франция",
+      img: "preview/francais/33-FR-I told you so.jpg",
+      desc: "Я же тебе говорил",
+    },
+    {
+      country: "Россия",
+      img: "preview/27-RU-I've had enough2.jpg",
+      desc: "С меня хватит; Раздраженный",
+    },
+  ],
+  Пойдем: [
+    {
+      country: "Франция",
+      img: "preview/francais/37-FR-Let's go.jpg",
+      desc: "Пойдем; Давай уйдем",
+    },
+    {
+      country: "Италия",
+      img: "preview/italy/17-IT-Let's go.jpg",
+      desc: "Пойдем; Давай уйдем",
+    },
+  ],
+  Страх: [
+    {
+      country: "Франция",
+      img: "preview/francais/35-FR-Are you scared-.jpg",
+      desc: "Ты боишься?; Страх",
+    },
+    {
+      country: "Италия",
+      img: "preview/italy/23-IT-Crowded place.jpg",
+      desc: "Многолюдное место; Полно людей",
+    },
+  ],
+  Нет: [
+    {
+      country: "Россия",
+      img: "preview/2-RU-No.jpg",
+      desc: "Нет; Не согласен",
+    },
+    {
+      country: "Болгария",
+      img: "preview/bolgaria/1-1-BU-Yes2.jpg",
+      desc: "Да",
+    },
+  ],
+  Да: [
+    {
+      country: "Россия",
+      img: "preview/1-RU-Yes.jpg",
+      desc: "Да; Я согласен",
+    },
+    {
+      country: "Болгария",
+      img: "preview/bolgaria/2-BU-No.jpg",
+      desc: "Нет; Не согласен",
+    },
+  ],
+  Фиг: [
+    {
+      country: "Болгария",
+      img: "preview/bolgaria/28-BU-You're getting anything.jpg",
+      desc: "Болгария ты получаешь что-нибудь; Ничего",
+    },
+    {
+      country: "Франция",
+      img: "preview/francais/36-FR-It won't happen.jpg",
+      desc: "Этого не случится; Забудь.",
+    },
+    {
+      country: "Португалия",
+      img: "preview/Portuges/8-PG-Good luck.jpg",
+      desc: "Удачи",
+    },
+  ],
+  Неверю: [
+    {
+      country: "Франция",
+      img: "preview/francais/20-FR-I don't believe you.jpg",
+      desc: "Я тебе не верю",
+    },
+    {
+      country: "Португалия",
+      img: "preview/Portuges/16-PG-Someone is smart.jpg",
+      desc: "Вы умны; Это умно!",
+    },
+  ],
+};
+document.addEventListener("DOMContentLoaded", function () {
+  const modal = document.getElementById("gestureModal");
+  const modalTitle = document.getElementById("modal-title");
+  const modalCardsContainer = document.getElementById("modal-cards-container");
+  const closeModal = document.querySelector(".close");
+
+  document.querySelectorAll(".video-card").forEach((card) => {
+    card.addEventListener("click", function () {
+      const gesture = this.dataset.gesture; // Ищем только по названию жеста
+      modalTitle.textContent = `Другие значения жеста`;
+      modalCardsContainer.innerHTML = "";
+
+      if (gestureVariations[gesture]) {
+        gestureVariations[gesture].forEach((variation) => {
+          modalCardsContainer.innerHTML += createCardHTML(variation);
+        });
+      } else {
+        modalCardsContainer.innerHTML =
+          "<p>Нет других значений для этого жеста.</p>";
+      }
+
+      modal.style.display = "block";
+    });
+  });
+
+  closeModal.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+
+  function createCardHTML(gesture) {
+    return `
+      <div class="modal-card">
+        <img src="${gesture.img}" alt="${gesture.desc}">
+        <p><strong>${gesture.country}:</strong> ${gesture.desc}</p>
+      </div>
+    `;
+  }
+});
